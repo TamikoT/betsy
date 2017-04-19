5bc76eabe7ff24790fde889c414b912182901c72
Kayla <kubicke.kayla@gmail.com>
2017-04-19 14:47:16 -0700

    Fixing typo.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  validates :username, presence: true, uniqueness: true, format: { with: /\A[-\w.]*\z/, message: ": Only accepts letters, numbers, period, hyphens and underscore allowed’" }
+  validates :username, presence: true, uniqueness: true, format: { with: /\A[-\w.]*\z/, message: ": Only accepts letters, numbers, period, hyphens and underscore allowed" }
   validates :email, presence: true, uniqueness: true, format: { with: /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i, message: "Email format." }
 
   has_many :products
