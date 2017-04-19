8918d34329c488c782dd995a7064d38df4de743c
Tamiko Terada <tamikodevo@gmail.com>
2017-04-19 10:59:05 -0700

    add UsersController specs to correct file

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,7 +1,36 @@
-require "test_helper"
+require 'test_helper'
 
 describe UsersController do
-  # it "must be a real test" do
-  #   flunk "Need real tests"
-  # end
+  describe "index" do
+    it "responds with no users" do
+      User.destroy_all
+
+      get users_path
+      must_respond_with :success
+    end
+
+    # TODO must create fixtures with many users
+    it "responds with multiple users" do
+    skip
+      User.count.must_be :>, 0
+
+      get users_path
+      must_respond_with :success
+    end
+  end
+
+  describe "show" do
+    it "responds with an existing user" do
+    skip
+      get user_path(User.first)
+      must_respond_with :success
+    end
+
+    it "404 with invalid user data" do
+    skip
+      bogus_user_id = User.last.id + 1
+      get user_path(bogus_user_id)
+      must_respond_with :not_found
+    end
+  end
 end
