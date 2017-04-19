5c4b40e2c73f91636d2ae6183d4978b168c7db8d
Kayla <kubicke.kayla@gmail.com>
2017-04-19 10:53:52 -0700

    Removed exta line.

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,6 @@ class User < ApplicationRecord
   has_many :user_orders
   has_many :orders, through: :user_orders
   has_many :reviews, through: :products
-  has_many :product_orders, through: :products
 
   has_many :categories, through: :product_categories
   has_many :reviews, through: :products
