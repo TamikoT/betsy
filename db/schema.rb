5d8c9f9f8728fc58a4e74206ad9c131a5e8a54e9
Danielle Birbal <birbalds@gmail.com>
2017-04-19 18:17:33 -0700

    Added login + logout via github

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170419014944) do
+ActiveRecord::Schema.define(version: 20170419211654) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -69,8 +69,10 @@ ActiveRecord::Schema.define(version: 20170419014944) do
   create_table "users", force: :cascade do |t|
     t.string   "username"
     t.string   "email"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",     null: false
+    t.datetime "updated_at",     null: false
+    t.string   "oauth_uid"
+    t.string   "oauth_provider"
   end
 
 end
