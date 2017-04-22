49e7ae9dbabcbbe584471b0511e6489ac1ab63b1
Kayla <kubicke.kayla@gmail.com>
2017-04-22 10:57:59 -0700

    Catergory can not be created unless the name is unique.

--- test/models/category_test.rb
+++ test/models/category_test.rb
@@ -1,11 +1,23 @@
 require "test_helper"
 
 describe Category do
-  # let(:category) { Category.new }
+  let(:category) { Category.new }
 
-  it "must be valid" do
+  it "all categories must be valid" do
     categories.each do |category|
       value(category).must_be :valid?
     end
   end
+
+  it "will not create new category without unique name" do
+    category_copy = category.dup
+    proc { category_copy.save! }.must_raise("Category name must be unique.")
+    category_copy.errors.must_include(:name)
+  end
 end
+
+# it 'must have unique pin' do
+#   person_copy = person.dup
+#   proc { person_copy.save! }.must_raise(Mongoid::Errors::Validations)
+#   person_copy.errors.must_include(:pin)
+# end
