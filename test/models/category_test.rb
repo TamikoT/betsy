a20200922941768e3087a1eec3cd69687926b38d
Kayla <kubicke.kayla@gmail.com>
2017-04-22 09:55:28 -0700

    First category test passing.

--- test/models/category_test.rb
+++ test/models/category_test.rb
@@ -1,9 +1,11 @@
 require "test_helper"
 
 describe Category do
-  let(:category) { Category.new }
+  # let(:category) { Category.new }
 
   it "must be valid" do
-    value(category).must_be :valid?
+    categories.each do |category|
+      value(category).must_be :valid?
+    end
   end
 end
