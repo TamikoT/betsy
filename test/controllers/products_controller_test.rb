f6c584b5a168fbf1862f48891ccfd0c2d6496941
Kayla <kubicke.kayla@gmail.com>
2017-04-22 10:40:30 -0700

    Added or removed an end or do to some of the describe blocks.

--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -2,7 +2,9 @@ require 'test_helper'
 
 describe ProductsController do
   describe "index" do
+
     it "responds with no product" do
+      skip
       Product.destroy_all
 
       get products_path
@@ -10,7 +12,7 @@ describe ProductsController do
     end
 
     it "responds with multiple products" do
-    skip
+      skip
       Product.count.must_be :>, 0
 
       get products_path
@@ -18,18 +20,25 @@ describe ProductsController do
     end
   end
 
-  describe new do; end
+  describe "new" do
+    it "makes new product" do
+      skip
+    end
+  end
 
-  describe create do
+
+  describe "create" do
     it "adds product to database" do
+      skip
       product_data = { product: { name: "Test Name", email: "test@aol.com" } }
       post products_path, params: product_data
       # T_T not necessarily where we will redirect to
       must_redirect_to products_path
     end
 
-    it "refreshed the form if the info sucks"
+    it "refreshed the form if the info sucks" do
       # test if no name is given
+      skip
       product_data = { email: { email: "noname@aol.com" } }
       post products_path, params: product_data
       must_respond_with :bad_request
@@ -38,13 +47,13 @@ describe ProductsController do
 
   describe "show" do
     it "responds with an existing product" do
-    skip
+      skip
       get product_path(Product.first)
       must_respond_with :success
     end
 
     it "404 with invalid product data" do
-    skip
+      skip
       bad_product_id = Product.last.id + 1
       get product_path(bad_product_id)
       must_respond_with :not_found
