180d02aafd6604323a99b95c2dfb99ba593089e3
Kayla <kubicke.kayla@gmail.com>
2017-04-23 10:08:00 -0700

    All orders created are valid.

--- test/models/order_test.rb
+++ test/models/order_test.rb
@@ -3,7 +3,14 @@ require "test_helper"
 describe Order do
   let(:order) { Order.new }
 
-  it "must be valid" do
-    value(order).must_be :valid?
+  it "all orders must be valid" do
+    orders.each do |order|
+      value(order).must_be :valid?
+    end
+  end
+
+  describe "validation tests" do
+    it  "new order can not be created without a status" do
+    end
   end
 end
