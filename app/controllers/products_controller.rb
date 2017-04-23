e8761c9d4f806ba3498bcb3b4f12e2df59525050
Danielle Birbal <birbalds@gmail.com>
2017-04-22 18:31:46 -0700

    Added category filter

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,35 +1,40 @@
 class ProductsController < ApplicationController
-  def index
-    @products = Product.all
-  end
+    def index
+        @categories = Category.all
+        if params[:category_id]
+            @products = Product.includes(:product_categories).where(product_categories: { category_id: params[:category_id] })
+        else
+            @products = Product.all
+        end
+    end
 
-  def create
-    Product.create(product_params)
+    def create
+        Product.create(product_params)
 
-    redirect_to products_path
-  end
+        redirect_to products_path
+    end
 
-  def new
-    @products = Product.new
-  end
+    def new
+        @products = Product.new
+    end
 
-  def edit
-    @product = Product.find(params[:id])
-  end
+    def edit
+        @product = Product.find(params[:id])
+    end
 
-  def show
-    @product = Product.find_by_id(params[:id].to_i)
-  end
+    def show
+        @product = Product.find_by_id(params[:id].to_i)
+    end
 
-  def update
-    product = Product.find(params[:id])
-    product.update(product_params)
-  end
+    def update
+        product = Product.find(params[:id])
+        product.update(product_params)
+    end
 
-  private
+    private
 
-  def product_params
-    return params.require(:product).permit(:stock, :name, :price, :user_id, :status)
-    #Did not add photos to product_params.
-  end
+    def product_params
+        params.require(:product).permit(:stock, :name, :price, :user_id, :status)
+        # Did not add photos to product_params.
+    end
 end
