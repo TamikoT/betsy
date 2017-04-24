cc6ca5b9197c4f82c0dfd0a5664858e260551883
Tamiko Terada <tamikodevo@gmail.com>
2017-04-23 18:45:10 -0700

    add initialize_cart before_action to app

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,11 +2,21 @@ class ApplicationController < ActionController::Base
     protect_from_forgery with: :exception
 
     before_action :find_user
+    before_action :initialize_cart
 
     private
 
     def find_user
         @current_user = User.find_by(id: session[:user_id]) if session[:user_id]
+    end
+
+    def initialize_cart
+      if session[:cart_id]
+        @cart = Cart.find(session[:cart_id])
+      else
+        @cart = Cart.create
+        session[:cart_id] = @cart.id
+      end
     end
 
     def require_login
