607272fdfaa4f6eb42b821bf0eef67e068d3b449
Kayla <kubicke.kayla@gmail.com>
2017-04-24 11:24:02 -0700

    Validation tests passing.

--- test/models/review_test.rb
+++ test/models/review_test.rb
@@ -4,6 +4,34 @@ describe Review do
   let(:review) { Review.new }
 
   it "must be valid" do
-    value(review).must_be :valid?
+    reviews.each do |review|
+      value(review).must_be :valid?
+    end
+  end
+
+  describe "validation tests" do
+    it "new review can not be created without a rating" do
+      proc { review.save! }.must_raise ("A review must have a rating.")
+      review.errors.must_include(:rating)
+    end
+
+    it "rating must be a number" do
+      review[:rating] = "string"
+      review.save
+
+      review.wont_be :valid?
+    end
+
+    it "rating must be a number between 1 and 5" do
+      review[:rating] = 7
+      review.save
+
+      review.wont_be :valid?
+    end
+  end
+
+  describe "association test" do
+    it "#product - test belongs to rlxn" do
+    end
   end
 end
