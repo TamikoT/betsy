104c7a8c83490969a3dfdcc6220e3306d93d4b40
Tamiko Terada <tamikodevo@gmail.com>
2017-04-24 15:11:19 -0700

    add product removed from order model

--- app/models/order.rb
+++ app/models/order.rb
@@ -7,14 +7,6 @@ class Order < ApplicationRecord
   has_many :user_orders
   has_many :users, through: :user_orders
 
-  def add_product(product_params)
-    current_item = ProductOrder.find_by(product_id: product_params[:product][:product_id])
-
-    if current_item
-      current_item.quantity += product_params[:product][:quantity].to_i
-      current_item.save!
-    else
-      ProductOrder.create(product_id: product_params[:product][:product_id], quantity: product_params[:product][:quantity], order_id: self.id )
-    end
+  def subtotal
   end
 end
