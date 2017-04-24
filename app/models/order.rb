099741e4935aa47cbc3a100a6e47e18004a83621
Leesyruiz <leesyruiz@gmail.com>
2017-04-24 16:40:22 -0700

    order fixed

--- app/models/order.rb
+++ app/models/order.rb
@@ -7,4 +7,11 @@ class Order < ApplicationRecord
   has_many :user_orders
   has_many :users, through: :user_orders
 
+
+  def subtotal
+    @cart.product_orders.each do |item|
+      item
+    end
+  end
+
 end
