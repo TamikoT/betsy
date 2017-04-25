76ca484e6f58f3edb128173bb574e6662c25646d
Tamiko Terada <tamikodevo@gmail.com>
2017-04-25 09:17:38 -0700

    add form for quantity for the cart

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -18,11 +18,19 @@ class OrdersController < ApplicationController
   def show
     @order = @cart # in ApplicationController
     @sum = cart_total
+    @items = cart_quantity
   end
 
   def add_product # passed in from product view
     new_line_item = ProductOrder.new(product_params)
     new_line_item.save!
+    redirect_to order_path
+  end
+
+  def update_quantity
+    line_item = ProductOrder.find_by_id(product_params[:product_id])
+    line_item.quantity = product_params[:quantity]
+    line_item.save!
     redirect_to order_path
   end
 
@@ -43,5 +51,13 @@ class OrdersController < ApplicationController
       sum += product.price * item.quantity
     end
     return sum
+  end
+
+  def cart_quantity
+    items = 0
+    @cart.product_orders.each do |item|
+      items += item.quantity
+    end
+    return items
   end
 end
