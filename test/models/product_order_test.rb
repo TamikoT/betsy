e993c5227dc03674bf7b17ad0c5a8dbd30198bac
Kayla <kubicke.kayla@gmail.com>
2017-04-26 08:57:43 -0700

    A product order can only have one product and one order.

--- test/models/product_order_test.rb
+++ test/models/product_order_test.rb
@@ -4,6 +4,33 @@ describe ProductOrder do
   let(:product_order) { ProductOrder.new }
 
   it "must be valid" do
-    value(product_order).must_be :valid?
+    product_orders.each do |product_order|
+      value(product_order).must_be :valid?
+    end
+  end
+  describe "validation tests" do
+    it "quantity can not be string" do
+      product_order[:quantity] = "String"
+      product_order.save
+
+      product_order.must_be :invalid?
+    end
+
+    it "quantity can not be 0" do
+      product_order[:quantity] = 0
+      product_order.save
+
+      product_order.must_be :invalid?
+    end
+  end
+
+  describe "association tests" do
+    it "#product" do
+      assert_equal 1, product_orders(:one).product_id
+    end
+
+    it "#order" do
+      assert_equal 1, product_orders(:one).order_id
+    end
   end
 end
