5655eade84c30da3ca1bc15488a8c073bde2f953
Tamiko Terada <tamikodevo@gmail.com>
2017-04-27 15:37:47 -0700

    move order calc to model

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,141 +1,125 @@
 class OrdersController < ApplicationController
-    before_action :initialize_cart, only: [:show, :edit, :update, :confirm]
+  before_action :initialize_cart, only: [:show, :edit, :update, :confirm]
 
-    def index
-        if params[:status]
-            @orders = Order.includes(:product_orders).where(product_orders: { product_id: Product.find_by(user_id: params[:user_id]).id }).where(status: params[:status])
-            @status_revenue = revenue_by_status(@orders, params[:user_id])
-        else
-            @orders = Order.includes(:product_orders).where(product_orders: { product_id: Product.find_by(user_id: params[:user_id]).id })
-            @status_revenue = revenue_by_status(@orders, params[:user_id])
-        end
+  def index
+    if params[:status]
+      @orders = Order.includes(:product_orders).where(product_orders: { product_id: Product.find_by(user_id: params[:user_id]).id }).where(status: params[:status])
+      @status_revenue = revenue_by_status(@orders, params[:user_id])
+    else
+      @orders = Order.includes(:product_orders).where(product_orders: { product_id: Product.find_by(user_id: params[:user_id]).id })
+      @status_revenue = revenue_by_status(@orders, params[:user_id])
     end
+  end
 
-    def create
-        Order.create(order_params)
+  def create
+    Order.create(order_params)
 
-        redirect_to orders_path
+    redirect_to orders_path
+  end
+
+  def new
+    @order = Order.new
+  end
+
+  def show
+    @order = @cart # in ApplicationController
+    @sum = @cart.cart_total
+    @items = @cart.cart_quantity
+  end
+
+  def edit
+    @order = @cart
+  end
+
+  def update
+    @order = @cart
+    @order.update(order_params)
+    @order.status = 'paid'
+    if @order.save
+      flash[:result_text] = "Your Order # #{@order.id} has been placed!"
+
+      @order.products.each do |product|
+        product.stock -= ProductOrder.find_by(order_id: @order.id, product_id: product.id).quantity
+        product.save
+      end
+
+      redirect_to confirmation_path
+    else
+      flash[:result_text] = 'Unable to place order'
+      flash[:messages] = @order.errors.messages
+      @order.status = 'pending'
+      render 'edit'
     end
+  end
 
-    def new
-        @order = Order.new
+  def remove_product
+    old_line_item = ProductOrder.find_by_id(params[:item_id].to_i)
+    old_line_item.destroy!
+
+    redirct_to order_path
+  end
+
+  def update_status
+    @order = Order.find_by(id: params[:order_id])
+    @order.status = params[:status]
+    @order.save
+    flash[:result_text] = "Successfluffy updated Order # #{@order.id} to #{@order.status}"
+    redirect_to :back
+  end
+
+  def confirm
+    @order = @cart
+    @sum = @cart.cart_total
+    @items = @cart.cart_quantity
+    session[:order_id] = nil if @order.status == 'paid'
+  end
+
+  def add_product # passed in from product view
+    prev_item = ProductOrder.find_by(product_params)
+    if prev_item
+      flash[:result_text] = "Added #{product_params[:quantity]} more #{Product.find_by(id: product_params[:product_id])} to cart"
+      prev_item.quantity += product_params[:quantity].to_i
+      prev_item.save!
+      redirect_to product_path(prev_item.product_id)
+    else
+      flash[:status] = :success
+      flash[:result_text] = "Successfluffy added #{Product.find_by(id: product_params[:product_id])} to cart"
+      ProductOrder.create!(product_params)
+      redirect_to order_path
     end
+  end
 
-    def show
-        @order = @cart # in ApplicationController
-        @sum = cart_total
-        @items = cart_quantity
+  def update_quantity
+    line_item = ProductOrder.find_by_id(params[:item_id].to_i)
+    line_item.quantity = params[:quantity].to_i
+
+    # T_T removes product from cart when qty is updated to 0
+    if line_item.quantity == 0
+      line_item.destroy!
+    else
+      line_item.save!
     end
+    redirect_to order_path
+  end
 
-    def edit
-        @order = @cart
+  def revenue_by_status(orders, user)
+    sum = 0.00
+    orders.each do |order|
+      order.product_orders.each do |item|
+        product = Product.find_by(id: item.product_id, user_id: user)
+        sum += product.price * item.quantity
+      end
     end
+    sprintf('%.2f', sum)
+  end
 
-    def update
-        @order = @cart
-        @order.update(order_params)
-        @order.status = 'paid'
-        if @order.save
-            flash[:result_text] = "Your Order # #{@order.id} has been placed!"
+  private
 
-            @order.products.each do |product|
-                product.stock -= ProductOrder.find_by(order_id: @order.id, product_id: product.id).quantity
-                product.save
-            end
+  def order_params
+    params.require(:order).permit(:status, :email_address, :mailing_address, :card_name, :card_expiration, :card_cvv, :zipcode)
+  end
 
-            redirect_to confirmation_path
-        else
-            flash[:result_text] = 'Unable to place order'
-            flash[:messages] = @order.errors.messages
-            @order.status = 'pending'
-            render 'edit'
-        end
-    end
-
-    def remove_product
-        old_line_item = ProductOrder.find_by_id(params[:item_id].to_i)
-        old_line_item.destroy!
-
-        redirct_to order_path
-    end
-
-    def update_status
-        @order = Order.find_by(id: params[:order_id])
-        @order.status = params[:status]
-        @order.save
-        flash[:result_text] = "Successfluffy updated Order # #{@order.id} to #{@order.status}"
-        redirect_to :back
-    end
-
-    def confirm
-        @order = @cart
-        @sum = cart_total
-        @items = cart_quantity
-        session[:order_id] = nil if @order.status == 'paid'
-
-        def add_product # passed in from product view
-            prev_item = ProductOrder.find_by(product_params)
-            if prev_item
-                flash[:result_text] = "Added #{product_params[:quantity]} more #{Product.find_by(id: product_params[:product_id])} to cart"
-                prev_item.quantity += product_params[:quantity].to_i
-                prev_item.save!
-                redirect_to product_path(prev_item.product_id)
-            else
-                flash[:status] = :success
-                flash[:result_text] = "Successfluffy added #{Product.find_by(id: product_params[:product_id])} to cart"
-                ProductOrder.create!(product_params)
-                redirect_to order_path
-            end
-        end
-
-        def update_quantity
-            line_item = ProductOrder.find_by_id(params[:item_id].to_i)
-            line_item.quantity = params[:quantity].to_i
-
-            # T_T removes product from cart when qty is updated to 0
-            if line_item.quantity == 0
-                line_item.destroy!
-            else
-                line_item.save!
-            end
-            redirect_to order_path
-        end
-
-        def revenue_by_status(orders, user)
-            sum = 0.00
-            orders.each do |order|
-                order.product_orders.each do |item|
-                    product = Product.find_by(id: item.product_id, user_id: user)
-                    sum += product.price * item.quantity
-                end
-            end
-            sprintf('%.2f', sum)
-        end
-
-        private
-
-        def order_params
-            params.require(:order).permit(:status, :email_address, :mailing_address, :card_name, :card_expiration, :card_cvv, :zipcode)
-        end
-
-        def product_params
-            params.permit(:product_id, :quantity, :order_id)
-        end
-
-        def cart_total
-            sum = 0.00
-            @cart.product_orders.each do |item|
-                product = Product.find_by_id(item.product_id)
-                sum += product.price * item.quantity
-            end
-            sum
-        end
-
-        def cart_quantity
-            items = 0
-            @cart.product_orders.each do |item|
-                items += item.quantity
-            end
-        end
-    end
+  def product_params
+    params.permit(:product_id, :quantity, :order_id)
+  end
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,21 +1,32 @@
 class Order < ApplicationRecord
-    validates :status, presence: true, inclusion: { in: %w(pending paid complete cancelled) }
-    validates :email_address, presence: true, format: { with: /\A[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]+\z/ }, if: "status == 'paid' || status == 'complete'"
-    validates :mailing_address, presence: true, if: "status == 'paid' || status == 'complete'"
-    validates :card_name, presence: true, if: "status == 'paid' || status == 'complete'"
-    validates :card_expiration, presence: true, if: "status == 'paid' || status == 'complete'"
-    validates :card_cvv, presence: true, length: { is: 3 }, if: "status == 'paid' || status == 'complete'"
-    validates :zipcode, presence: true, length: { is: 5 }, if: "status == 'paid' || status == 'complete'"
+  validates :status, presence: true, inclusion: { in: %w(pending paid complete cancelled) }
+  validates :email_address, presence: true, format: { with: /\A[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]+\z/ }, if: "status == 'paid' || status == 'complete'"
+  validates :mailing_address, presence: true, if: "status == 'paid' || status == 'complete'"
+  validates :card_name, presence: true, if: "status == 'paid' || status == 'complete'"
+  validates :card_expiration, presence: true, if: "status == 'paid' || status == 'complete'"
+  validates :card_cvv, presence: true, length: { is: 3 }, if: "status == 'paid' || status == 'complete'"
+  validates :zipcode, presence: true, length: { is: 5 }, if: "status == 'paid' || status == 'complete'"
 
-    has_many :product_orders
-    has_many :products, through: :product_orders
+  has_many :product_orders
+  has_many :products, through: :product_orders
 
-    has_many :user_orders
-    has_many :users, through: :user_orders
+  has_many :user_orders
+  has_many :users, through: :user_orders
 
-    def subtotal
-        @cart.product_orders.each do |item|
-            item
-        end
-    end
+  def cart_total
+      sum = 0.00
+      self.product_orders.each do |item|
+          product = Product.find_by_id(item.product_id)
+          sum += product.price * item.quantity
+      end
+      return sum
+  end
+
+  def cart_quantity
+      items = 0
+      self.product_orders.each do |item|
+          items += item.quantity
+      end
+      return items
+  end
 end
