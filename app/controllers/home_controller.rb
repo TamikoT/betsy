b06816ca7d2ca735f2d997f656e1e2d8f8365d46
Tamiko Terada <tamikodevo@gmail.com>
2017-04-27 15:54:03 -0700

    remove white space

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,16 +1,11 @@
 class HomeController < ApplicationController
 
   def index
-      @categories = Category.all
-      if params[:category_id]
-          @products = Product.includes(:product_categories).where(product_categories: { category_id: params[:category_id] })
-      else
-          @products = Product.all
-      end
+    @categories = Category.all
+    if params[:category_id]
+      @products = Product.includes(:product_categories).where(product_categories: { category_id: params[:category_id] })
+    else
+      @products = Product.all
+    end
   end
-
-
-
-
-
 end
