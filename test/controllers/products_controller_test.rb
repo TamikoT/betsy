458076f770ce71b12b86503fdcbf57ea328977fb
Kayla <kubicke.kayla@gmail.com>
2017-04-29 14:32:53 -0700

    Some failing tests now passing.

--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -1,10 +1,14 @@
 require 'test_helper'
 
 describe ProductsController do
+
+  before do
+    login(users(:grace))
+  end
   describe "index" do
 
     it "responds with no product" do
-      skip
+      # skip
       Product.destroy_all
 
       get products_path
@@ -12,7 +16,7 @@ describe ProductsController do
     end
 
     it "responds with multiple products" do
-      skip
+      # skip
       Product.count.must_be :>, 0
 
       get products_path
@@ -20,40 +24,54 @@ describe ProductsController do
     end
   end
 
-  describe "new" do
-    it "makes new product" do
-      skip
-    end
-  end
+  # describe "new" do
+  #   it "makes new product" do
+  #     skip
+  #   end
+  # end
 
 
   describe "create" do
     it "adds product to database" do
-      skip
-      product_data = { product: { name: "Test Name", email: "test@aol.com" } }
+      product_data =
+      { product:
+        {
+          name: "Test Name",
+          stock: 1,
+          price: 1.99
+        }
+      }
+
       post products_path, params: product_data
-      # T_T not necessarily where we will redirect to
-      must_redirect_to products_path
+      #Can't use @current_user because we are using a dummy login???
+      must_redirect_to user_path(id: session[:user_id])
     end
 
     it "refreshed the form if the info sucks" do
-      # test if no name is given
       skip
-      product_data = { email: { email: "noname@aol.com" } }
+      #TODO: Something wrong with actual redirect
+
+      product_data =
+      { product:
+        {
+          name: nil,
+          stock: 1,
+          price: 1.99
+        }
+      }
+
       post products_path, params: product_data
-      must_respond_with :bad_request
+      must_respond_with(:new)
     end
   end
 
   describe "show" do
     it "responds with an existing product" do
-      skip
       get product_path(Product.first)
       must_respond_with :success
     end
 
     it "404 with invalid product data" do
-      skip
       bad_product_id = Product.last.id + 1
       get product_path(bad_product_id)
       must_respond_with :not_found
