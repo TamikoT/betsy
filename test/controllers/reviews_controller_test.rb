644435d9fa2cffe97b47f490b5de22151a3a4381
Kayla <kubicke.kayla@gmail.com>
2017-05-01 13:31:37 -0700

    Index returns all reviews.

--- test/controllers/reviews_controller_test.rb
+++ test/controllers/reviews_controller_test.rb
@@ -1,7 +1,19 @@
 require "test_helper"
 
 describe ReviewsController do
-  # it "must be a real test" do
-  #   flunk "Need real tests"
-  # end
+  describe "index" do
+    it "returns all reviews" do
+      count = 0
+      reviews = Review.all
+      reviews_count = reviews.count
+
+      reviews.each do |review|
+        count += 1
+      end
+
+      reviews_count.must_equal count
+    end
+
+    
+  end
 end
